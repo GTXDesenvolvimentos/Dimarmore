d35e27a097fdd63f8285f807b790736d592ba37d
Marcio Batista da Silva <marcio.silva@cinpal.com>
2023-05-18 13:47:18 -0300

    Development

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -5,9 +5,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Desenvolvido pela comunidade X">
+    <meta name="description" content="Desenvolvido por GTX Software">
     <meta name="author" content="Márcio">
-    <title>Comunidade X</title>
+    <title>GTX Software</title>
     <link href="<?= base_url("assets/img/x.png") ?>" rel="icon" type="image/png">
     <!-- <link href="<?= base_url('/assets/css/all.min.css') ?>" rel="stylesheet" type="text/css"> -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
