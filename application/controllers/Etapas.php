49a8f9b2bd38e229f55c70b51b3b0cec018d7524
André Luiz Belmonte <belmonte_al@outlook.com>
2023-05-22 14:03:50 -0300

    Alterando a pagina etapas

---
+++ application/controllers/Etapas.php
@@ -0,0 +1,41 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Etapas extends MY_Controller
+{
+
+    ////////////////////////////////////////
+    // HOME PADRAO                     
+    // CRIADO POR MARCIO SILVA            
+    // DATA: 31/05/2019                   
+    ////////////////////////////////////////  
+    public function index()
+    {
+
+        $this->load->view('includes/header');
+        $this->load->view('includes/menu_sup');
+        $this->load->view('v_etapas');
+        $this->load->view('includes/modal');
+        $this->load->view('includes/footer');
+    }
+
+    ////////////////////////////////////////
+    // RETORNA DEPARTAMENTO                     
+    // CRIADO POR MARCIO SILVA            
+    // DATA: 08/02/2023                   
+    ////////////////////////////////////////
+    public function retEtapas()
+    {
+        $this->load->model('M_retorno');
+        $retorno = $this->M_retorno->retEtapas();
+        echo json_encode($retorno->result());
+    }
+
+    public function retUsers()
+    {
+        $this->load->model('M_retorno');
+        $retorno = $this->M_retorno->retUsers();
+        echo json_encode($retorno->result());
+    }
+}
