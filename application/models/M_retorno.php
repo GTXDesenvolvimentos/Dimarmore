684d9a597d14b9991c2d34595802fa846f02ed03
efelix2 <efelix64@hotmail.com>
2023-05-24 17:14:20 -0300

    criação de layout e modal

--- application/models/M_retorno.php
+++ application/models/M_retorno.php
@@ -88,4 +88,51 @@ class M_retorno extends CI_Model
 
         return $retorno;
     }
+
+    ////////////////////////////////////////
+    // RETORNO DE TODOS OS PROJETOS       //
+    // CRIADO POR ELIEL FELIX             //
+    // DATA: 22/05/2023                   //
+    ////////////////////////////////////////
+    public function retAtividades()
+    {
+        $this->db->select('*');
+        $this->db->select("DATE_FORMAT(dtcria, '%d/%m/%Y') AS dtcria", FALSE);
+        $this->db->select("DATE_FORMAT(data_fim, '%d/%m/%Y') AS data_fim", FALSE);
+        $this->db->where('status !=', 'D');
+        $retorno = $this->db->get('tbl_atividades');
+        return $retorno->result();
+    }
+
+    ////////////////////////////////////////
+    // RETORNO DE ATIVIDADES              //
+    // CRIADO POR MARCIO SILA             //
+    // DATA: 31/05/2019                   //
+    ////////////////////////////////////////
+    public function retAtividades2()
+    {
+        $id_atividade = $this->input->post('id_atividade');
+        $id_situacao = $this->input->post('id_situacao');
+        $this->db->select('A.situacao as situacao');
+        $this->db->select('A.id_atividade as id_atividade');
+        $this->db->select('A.id_projeto as id_projeto');
+        $this->db->select('A.atividade as nome_atividade');
+        $this->db->select('A.descricao as descricao_atividade');
+        $this->db->select("DATE_FORMAT(A.dtcria, '%d/%m/%Y') AS data_criacao", FALSE);
+        $this->db->select('C.nome as responsavel');
+        $this->db->select('A.anexo as anexo');
+        $this->db->select('A.prioridade as prioridade');
+        $this->db->select('B.descricao as descricao_projeto');
+        $this->db->select('B.nome as nome_projeto');
+        $this->db->select('D.descricao as nome_dep');
+        $this->db->join("tbl_projetos B", "A.id_projeto = B.id_projeto", "inner");
+        $this->db->join("tbl_users C", "A.id_usuario = C.id_users", "inner");
+        $this->db->join("tbl_departamentos D", "A.id_departamento = D.id_departamento", "inner");
+        isset($id_atividade) == true && $id_atividade != '' ? $this->db->where('A.id_atividade', $id_atividade) : '';
+        // isset($id_projeto) == true && $id_projeto != '' ? $this->db->where('A.id_projeto', $id_projeto) : '';
+        isset($id_situacao) == true && $id_situacao != '' ? $this->db->where('A.id_atividade', $id_situacao) : '';
+        $this->db->where('A.status !=', 'D');
+        $retorno = $this->db->get('tbl_atividades A');
+        return $retorno;
+    }
 }
