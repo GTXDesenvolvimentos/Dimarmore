9614fc2a0495fc1c632f69fd7ec98a942cab5eb8
efelix2 <efelix64@hotmail.com>
2023-07-27 17:14:02 -0300

    salvando base_url oficial como comentario para posterior uso

--- application/config/config.php
+++ application/config/config.php
@@ -23,6 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
+// URL oficial
+//https://gtxsoftware.com.br/projetos/dimarmore/ 
 $config['base_url'] = 'http://localhost/dimarmore/';
 /*
 |--------------------------------------------------------------------------
